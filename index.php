4324bc882355747837a18625498c0a80fe2c262c
JkDev <jkdevarg@gmail.com>
2023-02-18 20:39:22 -0500

    add leagueExp function

--- index.php
+++ index.php
@@ -6,15 +6,26 @@ $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
 //RANKED_SOLO_5x5
-$status = LeagueApi::challengerLeague(array(
+/* $status = LeagueApi::challengerLeague(array(
     'server' => 'la2',
     'game' => 'lol',
     'opt' => 'RANKED_SOLO_5x5',
-));
+)); */
 
 /* $status = LeagueApi::statusServer(array(
     'server' => 'la2',
     'game' => 'lol',
 )); */
 
-print_r($status);
+$status = LeagueApi::leagueExp(array(
+    'server' => 'la2',
+    'game' => 'lol',
+    'opt1' => 'RANKED_SOLO_5x5',
+    'opt2' => 'BRONZE',
+    'opt3' => 'I'
+));
+
+/* foreach ($status as $s){
+    print_r($s['summonerName'] .' <br> ');
+} */
+print_r(json_encode($status));
